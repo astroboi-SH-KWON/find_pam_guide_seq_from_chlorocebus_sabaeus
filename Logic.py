97f49503fe6788531a5accd74d01b305b0cec24c
astroboi <terry007x@korea.ac.kr>
2020-04-23 19:53:18 +0900

    기초

---
+++ Logic.py
@@ -0,0 +1,63 @@
+
+
+class Logics:
+    def __init__(self):
+        self.tmp = ""
+
+    def get_complementary(self, c):
+        if c == 'C':
+            return "G"
+        elif c == 'A':
+            return "T"
+        elif c == 'T':
+            return "A"
+        elif c == 'G':
+            return "C"
+        elif c == 'N':
+            return "N"
+        else:
+            print("get_complementary ERROR .... char is [" + c + "]")
+            exit()
+
+    """
+    match : match sequence with same length strings
+    :param
+        i : index of seq
+        dna_seq : targeted DNA/RNA sequence 
+        rule_str : rules with "ACGTU", "N", "R",...
+    :return
+        boolean
+    """
+    def match(self,i, dna_seq, rule_str):
+        if len(dna_seq) == i:
+            return True
+        if self.checkSeqByChar(dna_seq[i], rule_str[i]):
+            return self.match(i + 1, dna_seq, rule_str)
+        else:
+            return False
+
+    """
+    checkSeqByChar : match sequences by char with rules
+    :param
+        dna_char :
+        rule_char : rules with "A", "C", "G", "T", "U", "N", "R",...
+    :return
+        boolean
+    """
+    def checkSeqByChar(self,dna_char, rule_char):
+        flag = False
+        if rule_char == 'N':
+            return True
+        elif rule_char in 'ACGTU':
+            if dna_char == rule_char:
+                return True
+        elif rule_char == 'R':
+            if dna_char in 'AG':
+                return True
+        # elif rule_char == 'r':
+        #     if dna_char in 'CT':
+        #         return True
+        """
+        add more rules of "ACTGU"
+        """
+        return flag
